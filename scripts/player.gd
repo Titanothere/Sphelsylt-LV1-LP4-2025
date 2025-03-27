fa5228a396f6a7d46f38d28dd45d111c9383383a
Christoffer Ridderland <76886887+christoffer-ridderland@users.noreply.github.com>
2025-03-27 14:42:23 +0100

    fixed diagonal movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,23 +2,29 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-
+const SQRT2 = 1.414214
 
 func _physics_process(delta: float) -> void:
 
+    # Used to fix "fast diagonals"
+	var realSpeed = SPEED
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var hDirection := Input.get_axis("ui_left", "ui_right")
+	var vDirection := Input.get_axis("ui_up", "ui_down")
+
+	if hDirection and vDirection:
+		realSpeed /= SQRT2
+
 	if hDirection:
-		velocity.x = hDirection * SPEED
+		velocity.x = hDirection * realSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, realSpeed)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var vDirection := Input.get_axis("ui_up", "ui_down")
 	if vDirection:
-		velocity.y = vDirection * SPEED
+		velocity.y = vDirection * realSpeed
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, realSpeed)
 
 	move_and_slide()
