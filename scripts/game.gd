8c3c047c59e253efa9a3bafbd2085ac59b169038
Christoffer Ridderland <76886887+christoffer-ridderland@users.noreply.github.com>
2025-04-02 16:16:12 +0200

    Update game.gd

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,14 +1,15 @@
 extends Node2D
 @onready var stm = $StateMachine
 signal spawned
+var enemy
 func _process(delta : float) -> void:
 	connect("spawn", spawn_enemy)
 	pass
 func _ready() -> void:
+	enemy = preload("res://scenes/enemy.tscn").instantiate()
 	spawn_enemy()
 
 func spawn_enemy() -> void:
-	var enemy = load("res://scenes/enemy.tscn").instantiate()
 	add_child(enemy)
 	enemy.death.connect(stm.states["Combat"].on_death.bind())
 	spawned.emit()
