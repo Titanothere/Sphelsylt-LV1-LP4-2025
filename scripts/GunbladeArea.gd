c7d262d33735c62cd8decdc08a779b9d47a5f955
Christoffer Ridderland <76886887+christoffer-ridderland@users.noreply.github.com>
2025-04-02 14:36:00 +0200

    minor changes

--- scripts/GunbladeArea.gd
+++ scripts/GunbladeArea.gd
@@ -3,12 +3,18 @@ extends Area2D
 # Define the base parameters of the weapon
 var hitId = 0
 
+
+# aspect modifiers
 const SWING_SPEED = 1
 var swing_speed_mod = 1
 const SWING_DMG = 1
 var swing_dmg_mod = 1
 var rate_limit = 0
 var last_pos : Vector2
+var spread_mod = 1
+var attack_speed_mod = 1
+
+
 @onready var player = $""
 
 const weapon_offset = 0
@@ -34,15 +40,17 @@ func _physics_process(delta: float) -> void:
 		#print("swung")
 	event = Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
 	if event and rate_limit <= 0 and not anim.is_playing():
+		#Spawn Projectile
 		var projectile = load("res://scenes/bullet.tscn")
 		var bullet = projectile.instantiate()
 		var game = get_tree().get_root().get_node("Game")
 		game.add_child(bullet)
 		bullet.global_position = $"AnimatedSprite2D/BarrelExit".global_position
 		var rot : float = get_parent().rotation - PI
-		bullet.init_vals(rot, self.global_position - last_pos)
+		# Set Projectile parameters
+		bullet.init_vals(rot, self.global_position - last_pos, spread_mod)
 		last_pos = self.global_position
-		rate_limit = 1/bullet.bps
+		rate_limit = 1/(bullet.bps * attack_speed_mod)
 	else:
 		rate_limit -= delta
 
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,7 +1,9 @@
 extends Node2D
 @export var speed : float = 300
 var direction : Vector2
-var spread : float = 0.5
+
+const BASE_SPREAD : float = 0.5
+
 var bps : float = 5 # bullets per second
 var timeout : float = 1
 var dev : float = 1
@@ -9,8 +11,8 @@ var pp : Vector2 # this variable is really really stupid
 
 
 
-func init_vals(rot, ppos) -> void:
-	var real_rot = rot + randfn(0.0, 0.1*spread)
+func init_vals(rot, ppos, spread_mod) -> void:
+	var real_rot = rot + randfn(0.0, 0.1*BASE_SPREAD*spread_mod)
 	direction = Vector2(-cos(real_rot),-sin(real_rot)).normalized()
 	self.rotation = real_rot
 	pp = ppos
